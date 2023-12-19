6e720f49e82e6b23fb141b3c8add8a8cc5605c5a
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-19 19:04:30 -0300

    feat: add button to open sign up dialog

---
+++ components/signup/SignupDialog.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import signinContent from "~/utils/content/signin.content";
+
+const {$listen} = useNuxtApp()
+
+const showDialog = ref(false)
+
+const openDialog = () => {
+	console.log('test')
+	showDialog.value = true
+}
+
+const closeDialog = async () => {
+	showDialog.value = false
+}
+
+$listen('show-signup-dialog', openDialog)
+</script>
+
+<template>
+	<v-dialog width="400" v-model="showDialog">
+		<v-card>
+			<v-card-title class="d-flex align-center">
+				<v-btn
+					density="comfortable"
+					icon="mdi-close"
+					variant="plain"
+					class="mr-2"
+					@click="closeDialog()"
+				></v-btn>
+
+				<span>{{ signinContent.SIGNUP_DIALOG_TITLE }}</span>
+			</v-card-title>
+
+			<v-card-text></v-card-text>
+		</v-card>
+	</v-dialog>
+</template>
--- utils/content/signin.content.ts
+++ utils/content/signin.content.ts
@@ -1,5 +1,7 @@
 export default {
 	SIGNIN_CARD_TITLE: 'Drink Hub',
 	SIGNIN_FORM_SUBMIT_BUTTON: 'Entrar',
-	SIGNIN_ERROR_MESSAGE: 'E-mail ou senha inválidos'
+	SIGNIN_ERROR_MESSAGE: 'E-mail ou senha inválidos',
+	SIGNUP_DIALOG_TITLE: 'Cadastre-se',
+	SIGNUP_DIALOG_OPEN_BUTTON: 'Cadastre-se'
 }
