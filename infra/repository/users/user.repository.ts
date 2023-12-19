b9cc4ccaa9e15971099743438c60dcb3ef605302
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-19 16:47:35 -0300

    refactor: refactor UserRepository to fetch data correctly

--- infra/repository/users/user.repository.ts
+++ infra/repository/users/user.repository.ts
@@ -5,15 +5,18 @@ import type {UserToken} from "../../../domain/user/entities/user-token.entity";
 
 export default class UserRepository {
 
-	private apiHost: string = `${process.env.API_HOST_ADDRESS}/users`
+	private apiHost: string
+
+	constructor() {
+		const runTimeConfig = useRuntimeConfig()
+		this.apiHost = `${runTimeConfig.public.API_HOST_ADDRESS}/users`
+	}
 
 	public async signin (data: UserSignin): Promise<UserToken> {
-		const response: any = await $fetch(`${this.apiHost}/sign-in`, {
+		return await $fetch(`${this.apiHost}/sign-in`, {
 			method: 'post',
 			body: data
 		})
-
-		return { token: response.data.token }
 	}
 
 	public async signup (data: UserSignup): Promise<User> {
