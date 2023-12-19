d256a55b55aa9fffcd4be16014012db95d122655
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-19 20:00:13 -0300

    feat: add user pinia store

---
+++ infra/store/userStore.ts
@@ -0,0 +1,27 @@
+import {defineStore} from "pinia";
+import type {User} from "../../domain/user/entities/user.entity";
+import type {UserToken} from "../../domain/user/entities/user-token.entity";
+
+export const useUserStore = defineStore('user', {
+	state: () => {
+		return {
+			user: {},
+			token: ''
+		}
+	},
+	getters: {
+		getUser: state => state.user,
+		getToken: state => state.token
+	},
+	actions: {
+		saveUser(user: User) {
+			this.user = user
+		},
+		saveToken(data: UserToken) {
+			this.token = data.token
+		}
+	},
+	persist: {
+		storage: persistedState.localStorage,
+	}
+})
