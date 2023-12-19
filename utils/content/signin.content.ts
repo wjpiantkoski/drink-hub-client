18c8aeab618e787af10157d99cc8e48e17d2882c
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-19 19:33:14 -0300

    feat: add new content keys

--- utils/content/signin.content.ts
+++ utils/content/signin.content.ts
@@ -3,5 +3,8 @@ export default {
 	SIGNIN_FORM_SUBMIT_BUTTON: 'Entrar',
 	SIGNIN_ERROR_MESSAGE: 'E-mail ou senha inválidos',
 	SIGNUP_DIALOG_TITLE: 'Cadastre-se',
-	SIGNUP_DIALOG_OPEN_BUTTON: 'Cadastre-se'
+	SIGNUP_DIALOG_OPEN_BUTTON: 'Cadastre-se',
+	SIGNUP_ERROR_TITLE: 'Dados inválidos',
+	SIGNUP_ERROR_MESSAGE: 'Verifique seus dados e tente novamente',
+	SIGNUP_SUCCESS_MESSAGE: 'Cadastro realizado com sucesso'
 }
