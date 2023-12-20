0fb91ac033603a6923bf8403b9636569ac827091
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-20 13:39:32 -0300

    refactor: add image to beverage entity

--- domain/beverage/beverage.entity.ts
+++ domain/beverage/beverage.entity.ts
@@ -3,6 +3,7 @@ import type {Category} from "~/domain/category/entities/category.entity";
 export type Beverage = {
 	id: string
 	name: string
+	image: string,
 	userId: string,
 	category: Category
 }
