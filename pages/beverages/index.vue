207928e66299ea4364b53432a5e2d7e647cb61eb
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-20 10:04:25 -0300

    feat: add categories load at beverages page

--- pages/beverages/index.vue
+++ pages/beverages/index.vue
@@ -1,7 +1,38 @@
-<script lang="ts">
+<script lang="ts" setup>
+import CategoryClient from "~/infra/api-client/categories/category.client";
+import {useCategoryStore} from "~/infra/store/categoryStore";
+import categoryContent from "~/utils/content/category.content";
+
 definePageMeta({middleware: 'auth'})
+
+const {$event} = useNuxtApp()
+const categoryStore = useCategoryStore()
+
+const getCategories = async () => {
+	try {
+		$event('show-dialog-loader')
+
+		if (!categoryStore.getCategories.length) {
+			const categoryClient = new CategoryClient()
+			const categories = await categoryClient.getCategories()
+
+			categoryStore.saveCategories(categories)
+		}
+	} catch (err) {
+		$event('show-alert', {
+			type: 'error',
+			text: categoryContent.CATEGORY_LIST_ERROR
+		})
+	} finally {
+		$event('close-dialog-loader')
+	}
+}
+
+onMounted(async () => {
+	await getCategories()
+})
 </script>
 
 <template>
-	<h1>beverages</h1>
+<h1>teste</h1>
 </template>
