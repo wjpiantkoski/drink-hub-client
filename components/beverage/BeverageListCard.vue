1c685ba57df727e26707937aece5be3850d8df92
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-20 14:04:58 -0300

    feat: add button to show beverage details

--- components/beverage/BeverageListCard.vue
+++ components/beverage/BeverageListCard.vue
@@ -1,13 +1,26 @@
 <script lang="ts" setup>
 import type {Beverage} from "~/domain/beverage/beverage.entity";
 
+const {$event} = useNuxtApp()
+
 const {beverage} = defineProps<{
 	beverage: Beverage
 }>()
+
+const showBeverage = () => {
+	$event('show-beverage-dialog', {
+		beverageId: beverage.id
+	})
+}
 </script>
 
 <template>
-	<v-card hover color="grey-lighten-4" dark>
+	<v-card
+		dark
+		hover
+		color="grey-lighten-4"
+		@click="showBeverage()"
+	>
 		<v-card-title>
 			{{ beverage.name }}
 		</v-card-title>
