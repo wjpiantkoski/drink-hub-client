20d30274da3d46427fd6f3f27cc8383a2bc0e734
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-20 09:24:25 -0300

    feat: add logout

--- components/@shared/Toolbar.vue
+++ components/@shared/Toolbar.vue
@@ -1,5 +1,26 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import globalContent from "~/utils/content/global.content";
+import toolbarContent from "~/utils/content/toolbar.content";
+import {useUserStore} from "~/infra/store/userStore";
+import {useRouter} from "vue-router";
+
+const userStore = useUserStore()
+const router = useRouter()
+
+const logout = async () => {
+	userStore.saveToken()
+	await router.push('/signin')
+}
+</script>
 
 <template>
-	<v-toolbar></v-toolbar>
+	<v-toolbar color="primary" fixed>
+		<v-toolbar-title>{{ globalContent.APP_NAME }}</v-toolbar-title>
+
+		<v-spacer></v-spacer>
+
+		<v-btn variant="plain" @click="logout()">
+			{{ toolbarContent.TOOLBAR_LOGOUT_BUTTON }}
+		</v-btn>
+	</v-toolbar>
 </template>
