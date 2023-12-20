62c2c3f0f20bc1f49057f0a42b4575d1f456ccd4
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-20 10:03:12 -0300

    refactor: add authentication token to request

--- infra/api-client/categories/category.client.ts
+++ infra/api-client/categories/category.client.ts
@@ -1,15 +1,26 @@
 import type {Category} from "~/domain/category/entities/category.entity";
+import type {Store} from "pinia";
+import {useUserStore} from "~/infra/store/userStore";
 
 export default class CategoryClient {
 
 	private apiHost: string
+	private userStore
 
 	constructor() {
 		const runTimeConfig = useRuntimeConfig()
 		this.apiHost = `${runTimeConfig.public.API_HOST_ADDRESS}/categories`
+		this.userStore = useUserStore()
 	}
 
 	public async getCategories(): Promise<Category[]> {
-		return await $fetch(`${this.apiHost}`)
+		const token = this.userStore.getToken
+
+		return await $fetch(`${this.apiHost}`, {
+			method: 'get',
+			headers: {
+				Authorization: `bearer ${token}`
+			}
+		})
 	}
 }
