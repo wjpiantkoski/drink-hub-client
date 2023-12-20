1b12867f8036c0a6333e9fc31b14ad5d6f02526d
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-20 09:31:08 -0300

    feat: add categories store

---
+++ infra/store/categoryStore.ts
@@ -0,0 +1,21 @@
+import type {Category} from "~/domain/category/entities/category.entity";
+
+type CategoryState = {
+	categories: Category[]
+}
+
+export const useCategoryStore = defineStore('category', {
+	state: (): CategoryState => {
+		return {
+			categories: []
+		}
+	},
+	getters: {
+		getCategories: state => state.categories
+	},
+	actions: {
+		saveCategories(categories: Category[]) {
+			this.categories = categories
+		}
+	}
+})
