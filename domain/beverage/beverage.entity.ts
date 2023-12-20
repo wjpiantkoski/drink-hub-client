17fdb52adbf9cd5f719c3a104c3b28616413a62f
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-20 13:15:39 -0300

    refactor: update Beverage entity

--- domain/beverage/beverage.entity.ts
+++ domain/beverage/beverage.entity.ts
@@ -3,7 +3,6 @@ import type {Category} from "~/domain/category/entities/category.entity";
 export type Beverage = {
 	id: string
 	name: string
-	description: string
-	userId: string
+	userId: string,
 	category: Category
 }
