36f511a1a6e238b670633f79b397dfdc01fbd569
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-21 13:20:27 -0300

    feat: add create-beverage service

---
+++ domain/beverage/services/create-beverage.service.ts
@@ -0,0 +1,19 @@
+import type {BeverageBody} from "~/domain/beverage/entities/beverage-body.entity";
+import logoutService from "~/domain/user/services/logout.service";
+import createBeverageFormDataService from "~/domain/beverage/services/create-beverage-form-data.service";
+import BeveragesClient from "~/infra/api-client/beverages/beverages.client";
+
+export default async (beverageData: BeverageBody): Promise<void> => {
+	try {
+		const beverageFormData = createBeverageFormDataService(beverageData)
+		const beverageClient = new BeveragesClient()
+
+		await beverageClient.createBeverage(beverageFormData)
+	} catch (err: any) {
+		if (err.response.status === 401) {
+			await logoutService()
+		} else {
+			throw err
+		}
+	}
+}
