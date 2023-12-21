450ec2d35b683e7ef231075d9487037e93db37c3
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-21 08:07:29 -0300

    feat: add new beverage content key

--- utils/content/beverage.content.ts
+++ utils/content/beverage.content.ts
@@ -1,4 +1,8 @@
 export default {
 	BEVERAGES_PAGE_TITLE: 'Bebidas',
-	BEVERAGES_ADD_BUTTON_TITLE: 'Adicionar'
+	BEVERAGES_ADD_BUTTON_TITLE: 'Adicionar',
+	BEVERAGE_INSERT_DIALOG_TITLE: 'Adicionar bebida',
+	BEVERAGE_UPDATE_DIALOG_TITLE: 'Editar bebida',
+	BEVERAGE_SUBMIT_BUTTON_TITLE: 'Salvar',
+	BEVERAGE_SUBMIT_ERROR_MESSAGE: 'Falha ao salvar bebida'
 }
