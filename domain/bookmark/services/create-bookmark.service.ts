8af7d0e43390963f55e302a1acf8d666a190e691
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-21 13:40:08 -0300

    refactor: remove event trigger from service

--- domain/bookmark/services/create-bookmark.service.ts
+++ domain/bookmark/services/create-bookmark.service.ts
@@ -1,12 +1,9 @@
 import type {BookmarkBody} from "~/domain/bookmark/entities/bookmark-body.entity";
 import logoutService from "~/domain/user/services/logout.service";
-import bookmarkContent from "~/utils/content/bookmark.content";
 import BookmarkClient from "~/infra/api-client/bookmarks/bookmark.client";
 import getBookmarksService from "~/domain/bookmark/services/get-bookmarks.service";
 
 export default async (bookmark:BookmarkBody) => {
-	const {$event} = useNuxtApp()
-
 	try {
 		const bookmarkClient = new BookmarkClient()
 
@@ -15,11 +12,6 @@ export default async (bookmark:BookmarkBody) => {
 	} catch (err: any) {
 		if (err.response.status === 401) {
 			await logoutService()
-		} else {
-			$event('show-alert', {
-				type: 'error',
-				text: bookmarkContent.BOOKMARK_ADD_ERROR_MESSAGE
-			})
 		}
 	}
 }
