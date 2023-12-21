3d557893915670f50fad75bb43033c78eff78fe2
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-21 10:35:36 -0300

    feat: add get-beverages service

---
+++ domain/beverage/services/get-beverages.service.ts
@@ -0,0 +1,16 @@
+import type {Beverage} from "../../../domain/beverage/entities/beverage.entity";
+import logoutService from "../../../domain/user/services/logout.service";
+import BeveragesClient from "../../../infra/api-client/beverages/beverages.client";
+
+export default async (categoryId: string): Promise<Beverage[]> => {
+	try {
+		const beveragesClient = new BeveragesClient()
+		return await beveragesClient.getBeveragesByCategory(categoryId)
+	} catch (err: any) {
+		if (err.response.status === 401) {
+			await logoutService()
+		}
+	}
+
+	return []
+}
