a7f1abc4db07c3de886ca090b45f2dcd741f2f1e
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-21 13:41:27 -0300

    refactor: remove unecessary code from user service

--- domain/user/services/logout.service.ts
+++ domain/user/services/logout.service.ts
@@ -1,7 +1,3 @@
-import {useUserStore} from "~/infra/store/userStore";
-import {useBookmarkStore} from "~/infra/store/bookmarkStore";
-import {useRouter} from "vue-router";
-
 export default async () => {
 	const {$event} = useNuxtApp()
 	$event('logout')
