c918fa24946684f1a728913e53de4e54322bb7f2
wjpiantkoski <wjpiantkoski@gmail.com>
2023-12-21 13:43:45 -0300

    refactor: refactor BeverageRemoveDialog to use defineComponent

--- components/beverage/BeverageRemoveDialog.vue
+++ components/beverage/BeverageRemoveDialog.vue
@@ -1,51 +1,64 @@
-<script lang="ts" setup>
+<script lang="ts">
 import beverageContent from "~/utils/content/beverage.content";
 import BeveragesClient from "~/infra/api-client/beverages/beverages.client";
+import removeBeverageService from "~/domain/beverage/services/remove-beverage.service";
 
-const {$event, $listen} = useNuxtApp()
+export default defineComponent({
+	setup() {
+		const {$event, $listen} = useNuxtApp()
 
-const beverage: any = ref(null)
-const removeBeverageLoader = ref(false)
-const showRemoveBeverageDialog = ref(false)
+		const beverage: any = ref(null)
+		const removeBeverageLoader = ref(false)
+		const showRemoveBeverageDialog = ref(false)
 
-const openDialog = (data: any) => {
-	beverage.value = data
-	showRemoveBeverageDialog.value = true
-}
+		const openDialog = (data: any) => {
+			beverage.value = data
+			showRemoveBeverageDialog.value = true
+		}
 
-const closeDialog = () => {
-	showRemoveBeverageDialog.value = false
-	beverage.value = null
-}
+		const closeDialog = () => {
+			showRemoveBeverageDialog.value = false
+			beverage.value = null
+		}
 
-const removeBeverage = async () => {
-	try {
-		removeBeverageLoader.value = true
+		const removeBeverage = async () => {
+			try {
+				removeBeverageLoader.value = true
 
-		const beverageClient = new BeveragesClient()
-		await beverageClient.removeBeverage(beverage.value.id)
+				await removeBeverageService(beverage.value.id)
 
-		$event('refresh-beverages-list', {
-			categoryId: beverage.value.category.id
-		})
+				$event('refresh-beverages-list', {
+					categoryId: beverage.value.category.id
+				})
 
-		$event('show-alert', {
-			type: 'success',
-			text: beverageContent.BEVERAGE_REMOVE_SUBMIT_SUCCESS_MESSAGE
-		})
+				$event('show-alert', {
+					type: 'success',
+					text: beverageContent.BEVERAGE_REMOVE_SUBMIT_SUCCESS_MESSAGE
+				})
 
-		closeDialog()
-	} catch {
-		$event('show-alert', {
-			type: 'error',
-			text: beverageContent.BEVERAGE_REMOVE_SUBMIT_ERROR_MESSAGE
-		})
-	} finally {
-		removeBeverageLoader.value = false
+				closeDialog()
+			} catch {
+				$event('show-alert', {
+					type: 'error',
+					text: beverageContent.BEVERAGE_REMOVE_SUBMIT_ERROR_MESSAGE
+				})
+			} finally {
+				removeBeverageLoader.value = false
+			}
+		}
+
+		$listen('show-dialog-remove-beverage', openDialog)
+
+		return {
+			showRemoveBeverageDialog,
+			beverage,
+			closeDialog,
+			removeBeverageLoader,
+			beverageContent,
+			removeBeverage
+		}
 	}
-}
-
-$listen('show-dialog-remove-beverage', openDialog)
+})
 </script>
 
 <template>
